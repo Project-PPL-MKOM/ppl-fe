8c31c034cf6cdbb0740693f446efdd321e5c9fb3
waffiq <waffiqmaaroja1998@gmail.com>
2024-05-20 22:18:40 +0700

    create assets and theme

---
+++ lib/core/assets/constants.dart
@@ -0,0 +1,5 @@
+abstract class AppAssets {
+  static const imgPlan = 'assets/images/plan.png';
+  static const imgBabyMeasure = 'assets/images/baby_measure.png';
+  static const imgHeightTrend = 'assets/images/height_trend.png';
+}
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppTheme {
+  static final family = GoogleFonts.inter(color: dark);
+  static const primary100 = Color(0xFFE3F2FD);
+  static const primary400 = Color(0xFF42A5F5);
+  static const primary800 = Color(0xFF1976D2);
+  static const pink100 = Color(0xFFFDE3FC);
+  static const pink400 = Color(0xFFF542F5);
+  static const dark = Color(0xFF212325);
+  static const grey = Color(0xFF8F8E94);
+}
