fbb12d16b06aebb6dce150848fe93cb60b7e6862
waffiq <waffiqmaaroja1998@gmail.com>
2024-05-20 22:19:39 +0700

    create dashboard presentation

---
+++ lib/presentation/dashboard/view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DashboardView extends GetView<DashboardView> {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,41 @@
+import 'package:get/get.dart';
+import 'package:project/presentation/dashboard/dashboard.dart';
+import 'package:project/presentation/home/home.dart';
+import 'package:project/presentation/landing/landing.dart';
+import 'package:project/presentation/profile/profile.dart';
+import 'package:project/presentation/splashscreen/splashscreen.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  static const initial = AppRoutes.root;
+
+  static final routes = [
+    GetPage(
+      name: AppRoutes.root,
+      page: () => const SplashscreenView(),
+      binding: SplashscreenBinding(),
+    ),
+    GetPage(
+      name: AppRoutes.landing,
+      page: () => LandingView(),
+      binding: LandingBinding(),
+      // middlewares: [LandingMiddleware()],
+    ),
+    GetPage(
+      name: AppRoutes.home,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: AppRoutes.profile,
+      page: () => const ProfileView(),
+      binding: ProfileBinding(),
+    ),
+    GetPage(
+      name: AppRoutes.dashboard,
+      page: () => const DashboardView(),
+      binding: DashboardBinding(),
+    ),
+  ];
+}
